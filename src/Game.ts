bc3b0a04aa65ff8e077f818f9318f3b6e7256d40
brendino500 <brenda.ty@live.com>
2020-10-04 20:07:42 +0100

    fire action registered

--- src/Game.ts
+++ src/Game.ts
@@ -1,6 +1,7 @@
 import * as PIXI from "pixi.js";
 import Hero from "./Hero";
 import Fire from "./Fire";
+import Enemy from "./Enemy";
 import TWEEN from "@tweenjs/tween.js";
 
 export default class Game {
@@ -9,6 +10,7 @@ export default class Game {
   private gameHeight: number;
   private hero: Hero;
   private fireArray: Fire[] = [];
+  private enemyArray: Enemy[] = [];
 
   constructor(stage: PIXI.Container, gameWidth: number, gameHeight: number) {
     this.stage = stage;
@@ -17,6 +19,7 @@ export default class Game {
     this.hero = new Hero(PIXI.Texture.from("birdUp.png"), gameWidth, gameHeight);
     this.stage.addChild(this.hero);
     this.createListeners();
+    this.createEnemies();
   }
 
   private createListeners(): void {
@@ -36,6 +39,16 @@ export default class Game {
       this.destroyFire(fire);
       console.log(this.fireArray.length);
     });
+    tween.onUpdate(() => {
+      this.enemyArray.forEach((enemy) => {
+        const yOverlap =
+          fire.y - fire.height / 2 < enemy.y + enemy.height / 2 &&
+          fire.y + fire.height / 2 > enemy.y - enemy.height / 2;
+        const xOverlap =
+          fire.x - fire.width / 2 < enemy.x + enemy.width / 2 && fire.x + fire.width / 2 > enemy.x - enemy.width / 2;
+        console.log(yOverlap, xOverlap, yOverlap && xOverlap);
+      });
+    });
     tween.start(performance.now());
     console.log("engelbert", tween);
   }
@@ -44,5 +57,17 @@ export default class Game {
     this.stage.removeChild(fire);
     const index = this.fireArray.indexOf(fire);
     this.fireArray.splice(index, 1);
+  }
+
+  private createEnemies(): void {
+    const enemy = new Enemy(
+      PIXI.Texture.from("birdUp.png"),
+      this.gameWidth,
+      this.gameHeight,
+      this.gameWidth / 2,
+      this.gameHeight / 4
+    );
+    this.stage.addChild(enemy);
+    this.enemyArray.push(enemy);
   }
 }
